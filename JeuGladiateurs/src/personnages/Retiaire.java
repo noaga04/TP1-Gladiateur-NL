c2cd24957391d3a2ba9384d39aedbde6c89dda01
NoaLebreton <noa.lebreton@gmail.com>
2023-09-13 16:06:53 -0400

    test avant fin

--- JeuGladiateurs/src/personnages/Retiaire.java
+++ JeuGladiateurs/src/personnages/Retiaire.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package personnages;
-
+import java.util.Random;
 /**
  *
  * @author 202230225
@@ -11,6 +11,7 @@ package personnages;
 public class Retiaire extends Personnage {
     
     String classPersonnage;
+    boolean possedeFilet = true;
     
     public Retiaire(String nomP, int pointAttaque, int pointDefense, int pointDeVieP, int initiativeP) {
         super(nomP, pointAttaque, pointDefense, pointDeVieP, initiativeP);
@@ -21,5 +22,29 @@ public class Retiaire extends Personnage {
     public void afficherInfosPersonnage() {
         super.afficherInfosPersonnage();
         System.out.println("    Classe : " + classPersonnage);
+    }
+    
+    @Override
+    public void frapperPersonnage(Personnage personnageCible) {
+            
+        if (possedeFilet == true){
+            System.out.println("Igor l'empaleur lance son filet...");
+            Random rand = new Random();
+            int minValue = 0;
+            int maxValue = 10;
+            int nbRand = rand.nextInt(maxValue - minValue) + minValue;
+            if (nbRand == 5){
+                System.out.println("Son filet attrape Bob le malchanceux et il l’empale sauvagement avec sa lance !");
+                personnageCible.setPointDeVie(0);
+            } 
+            else{
+                System.out.println("Le filet n'atteint pas sa cible ...");
+                possedeFilet = false;
+            }
+        }
+        else{
+            System.out.println("Igor l'empaleur ramasse son filet et en profite pour attaquer");
+            possedeFilet = true;
+        }
     }
 }
